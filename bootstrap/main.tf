6480fa205c7eab2a1669ee16b938f0e85a5394af
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-05-01 22:42:27 +0200

    Added bootstrap terraform file

---
+++ bootstrap/main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      "owner"         = "jesse.naidoo@bbd.co.za"
+      "created-using" = "terraform"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "234642354232-state"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "234642354232-state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+data "tls_certificate" "githubActions" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_openid_connect_provider" "githubActions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = distinct(concat(data.tls_certificate.githubActions.certificates[*].sha1_fingerprint, ["1b511abead59c6ce207077c0bf0e0043b1382612", "959cb2b52b4ad201a593847abca32ff48f838c2e", "6938fd4d98bab03faadb97b34396831e3780aea1"]))
+}
+
+resource "aws_iam_role" "githubActions" {
+  name                 = "GithubRunner"
+  assume_role_policy   = local.trust_policy
+  managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  max_session_duration = 3600
+}
+
+output "githubActionsRole" {
+  value = aws_iam_role.githubActions.arn
+}
+
+locals {
+  trust_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Federated" : "arn:aws:iam::234642354232:oidc-provider/token.actions.githubusercontent.com"
+          },
+          "Action" : "sts:AssumeRoleWithWebIdentity",
+          "Condition" : {
+            "StringEquals" : {
+              "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+            },
+            "StringLike" : {
+              "token.actions.githubusercontent.com:sub" : "repo:Spyderware/spyder:*"
+            }
+          }
+        }
+      ]
+    }
+  )
+}
