823566fbe9fdbe5d11e831ecfe16700352c56a64
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-05-03 20:55:43 +0200

    Fixed Issue with keyword

--- sql/V202404300922__DbInit.sql
+++ sql/V202404300922__DbInit.sql
@@ -1,6 +1,6 @@
-CREATE TABLE user (
-  user_id char(20),
-  username varchar(25)
+CREATE TABLE account (
+  account_id char(20),
+  accountname varchar(25)
 );
 
 CREATE TABLE category (
@@ -10,7 +10,7 @@ CREATE TABLE category (
 
 CREATE TABLE post (
   post_id serial,
-  user_id int,
+  account_id int,
   title varchar(35),
   body varchar(255),
   category_id int,
@@ -19,7 +19,7 @@ CREATE TABLE post (
 
 CREATE TABLE comment (
   comment_id serial,
-  user_id int,
+  account_id int,
   post_id int,
   comment varchar(50),
   removed bool
@@ -27,7 +27,7 @@ CREATE TABLE comment (
 
 CREATE TABLE post_upvote (
   post_interaction_id serial,
-  user_id int,
+  account_id int,
   post_id int
 );
 
@@ -35,16 +35,16 @@ ALTER TABLE post_upvote
 ADD CONSTRAINT post_upvote_post_id_fk FOREIGN KEY (post_id) REFERENCES post (post_id);
 
 ALTER TABLE post_upvote 
-ADD CONSTRAINT post_upvote_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id);
+ADD CONSTRAINT post_upvote_account_id_fk FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE post 
 ADD CONSTRAINT post_category_id_fk FOREIGN KEY (category_id) REFERENCES category (category_id);
 
 ALTER TABLE post 
-ADD CONSTRAINT post_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id);
+ADD CONSTRAINT post_account_id_fk FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE comment 
-ADD CONSTRAINT comment_user_id_fk FOREIGN KEY (user_id) REFERENCES user (user_id);
+ADD CONSTRAINT comment_account_id_fk FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE comment 
 ADD CONSTRAINT comment_post_id_fk FOREIGN KEY (post_id) REFERENCES post (post_id);
