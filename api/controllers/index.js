eb176226522a0ae9c43af8667dd07c35e52dd0e4
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-05-06 21:29:43 +0200

    Completed comment CRUD

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,5 +2,6 @@ import * as AccountController from "./account.controller.js";
 import * as CategoryController from "./category.controller.js";
 import * as PostController from "./post.controller.js";
 import * as PostUpvoteController from "./post_upvote.controller.js";
+import * as CommentController from "./comment.controller.js";
 
-export {AccountController, CategoryController, PostController, PostUpvoteController};
+export {AccountController, CategoryController, PostController, PostUpvoteController, CommentController};
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ import {accountRouter} from './account.route.js';
 import {categoryRouter} from './category.route.js';
 import {postRouter} from './post.route.js';
 import {postUpvoteRouter} from './post_upvote.route.js';
+import {commentRouter} from './comment.route.js';
 import {Router} from "express";
 
 export const router = Router();
@@ -10,4 +11,5 @@ router.use('/account', accountRouter);
 router.use('/category', categoryRouter);
 router.use('/post', postRouter);
 router.use('/postUpvote', postUpvoteRouter);
+router.use('/comment', commentRouter);
 
