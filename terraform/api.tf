a98f6d30d4e84534435bf7660bffda434139477a
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-05-07 15:19:53 +0200

    Added API setup

--- terraform/api.tf
+++ terraform/api.tf
@@ -3,6 +3,11 @@ resource "aws_s3_bucket" "beanstalk_file_bucket" {
   force_destroy = true
 }
 
+# resource "aws_acm_certificate" "https_cert" {
+#   domain_name       = "spyder-api-env.eba-zuxdcz8g.eu-west-1.elasticbeanstalk.com"
+#   validation_method = "DNS"
+# }
+
 resource "aws_iam_role" "beanstalk_ec2" {
   assume_role_policy    = jsonencode(
     {
@@ -54,51 +59,127 @@ resource "aws_elastic_beanstalk_environment" "beanstalk_env" {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
     value     = module.vpc.vpc_id
+    resource  = ""
   }
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.beanstalk_ec2.name
+    resource  = ""
   }
   setting {
     namespace = "aws:ec2:vpc"
     name      = "Subnets"
     value     = join(",", module.vpc.public_subnets)
+    resource  = ""
   }
   setting {
     namespace = "aws:ec2:instances"
     name      = "InstanceTypes"
     value     = "t3.micro"
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:healthreporting:system"
     name      = "SystemType"
     value     = "basic"
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:application"
     name      = "Application Healthcheck URL"
     value     = "/"
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "Timeout"
     value     = "60"
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:command"
     name      = "IgnoreHealthCheck"
     value     = "true"
-  }
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "SingleInstance"
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:managedactions"
     name      = "ManagedActionsEnabled"
     value     = "false"
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elbv2:listener:80"
+    name      = "ListenerEnabled"
+    value     = "true"
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elbv2:listener:80"
+    name      = "Protocol"
+    value     = "HTTP"
+    resource  = ""
+  }
+  # setting {
+  #   namespace = "aws:elbv2:listener:443"
+  #   name      = "ListenerEnabled"
+  #   value     = "true"
+  #   resource  = ""
+  # }
+  # setting {
+  #   namespace = "aws:elbv2:listener:443"
+  #   name      = "Protocol"
+  #   value     = "HTTPS"
+  #   resource  = ""
+  # }
+  # setting {
+  #   namespace = "aws:elbv2:listener:443"
+  #   name      = "SSLCertificateArns"
+  #   value     = "arn:aws:acm:eu-west-1:574836245203:certificate/0695baa7-6cd3-4fc2-a6a5-960ee3dc9e91"
+  #   resource  = ""
+  # }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     = "true"
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elb:healthcheck"
+    name      = "Interval"
+    value     = 60
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:elb:healthcheck"
+    name      = "Timeout"
+    value     = 20
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+    resource  = ""
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 1
+    resource  = ""
   }
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
