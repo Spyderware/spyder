cd367441544886e1ec8d9ffbf26bdb2bec09a498
Tristan Phipson <tristan.phipson@bbd.co.za>
2024-05-09 14:18:46 +0200

    feature/SWLU-35: added API int.

--- web/scripts/api.js
+++ web/scripts/api.js
@@ -1,32 +1,23 @@
-const BASE_API_URL = 'https://api.spyder.phipson.co.za';
-const API_PATH = '/api/v1/';
-
-async function getPostById(postId) {
-    const query = `${BASE_API_URL}${API_PATH}post/${postId}`;
-    const response = await fetch(query);
-    const responseData = await response.json();
-
-    return responseData;
+import { BASE_API_URL } from "./config.js";
+ 
+function postData(endpoint, payload, jwt) {
+    return fetch(`${BASE_API_URL}/${endpoint}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`
+        },
+        body: payload
+    });
 }
-//TODO : try catches
-async function getUsernameById(accountId) {
-    const query = `${BASE_API_URL}${API_PATH}account/${accountId}`;
-    const response = await fetch(query);
-    const responseData = await response.json();
-    return responseData.username;
+ 
+function getData(endpoint, jwt) {
+    return fetch(`${BASE_API_URL}/${endpoint}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`
+        }
+    });
 }
-
-async function getPostContent(postId) {
-    const postData = await getPostById(postId);
-    const postUsername = await getUsernameById(postData.account_id);
-    const result = {
-        postId: postId,
-        username: postUsername,
-        title: postData.title,
-        body: postData.body
-    }
-
-    return result;
-}
-
-export { getPostContent }
+ 
+export { getData, postData }
--- web/scripts/home.js
+++ web/scripts/home.js
@@ -1,19 +1,44 @@
+import { getData } from "./api.js";
+import { retrieveJWT } from "./auth.js";
 import { changeRoute } from "./router.js";
 import { createPostFromTemplate } from "./template-loader.js";
 
+// =================== Init ===================
+addEventListener('home-init', initPage);
+initPage();
+
 // =================== Constants ===================
 const HOMEPAGE_MAIN_VIEW_ID = 'homepage-main-view';
+const HOMEPAGE_NO_POSTS_ID = 'home-no-posts';
 
 // =================== Functions ===================
+
+async function initPage() {
+    const jwt = retrieveJWT();
+    const response = await getData('post', jwt);
+    let posts = [];
+    try {
+        posts = await response.json();
+    } catch (error) {
+        console.error('Could not fetch posts.');
+    }
+
+    if (posts.length > 0) {
+        populatePostsList(posts);
+    } else {
+        document.getElementById(HOMEPAGE_NO_POSTS_ID).style.display = 'flex';
+    }
+}
 
 /**
  * This function takes a list of post data objects, and dynamically creates the post `cards`
  * that get added to the homepage.
  * @param {{
- * postId,
+ * post_id,
 * username,
 * title,
-* body
+* body,
+* umg_url,
 * }[]} posts - a list of posts with their data.
  */
 async function populatePostsList(posts) {
@@ -26,42 +51,10 @@ async function populatePostsList(posts) {
             const postHtmlElement = await createPostFromTemplate(post);
             postContainer.insertAdjacentHTML('beforeend', postHtmlElement);
 
-            document.getElementById(`post-${post.postId}`).addEventListener('click', function (event) {
+            document.getElementById(`post-${post.post_id}`).addEventListener('click', function (event) {
                 event.preventDefault();
                 changeRoute(event.currentTarget.getAttribute('href'), false);
             });
         }
     }
 }
-
-// ------------------------------- TESTING CODE -------------------------------
-
-const testPosts = [
-    {
-        postId: 1,
-        username: 'Trucks<3',
-        title: 'Post 1 Baby!',
-        body: "Smelling cheese is the first sign of major success."
-    },
-    {
-        postId: 2,
-        username: 'RandyFlacks29',
-        title: 'Post 2 Amazing Title',
-        body: "Wow this exploit blew up Cpt. Jack Sparrow's back vault."
-    },
-    {
-        postId: 3,
-        username: 'Po7en7',
-        title: 'Post 3: Rise of the Cupcakes',
-        body: "In a quaint village nestled between rolling hills and winding rivers, life unfolded like pages in a storybook. Narrow cobblestone streets wound their way through the town, lined with colorful cottages adorned with flower boxes bursting with blooms. The scent of freshly baked bread wafted from the local bakery, mingling with the earthy aroma of the nearby forest. Villagers bustled about their daily routines, exchanging smiles and friendly greetings as they went about their tasks. At the heart of the village stood an ancient oak tree, its gnarled branches stretching towards the sky like outstretched arms, offering shade and shelter to those who sought respite beneath its boughs. As the sun dipped low on the horizon, painting the sky in hues of orange and pink, the village settled into a peaceful rhythm, a timeless haven untouched by the chaos of the outside world."
-    },
-    {
-        postId: 4,
-        username: 'WoNiLi',
-        title: 'Post 4 - Wall Hacks',
-        body: "Pineapples everywhere!"
-    }
-];
-populatePostsList(testPosts);
-
-// ------------------------------- END TESTING CODE -------------------------------
